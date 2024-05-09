185bb39f7bc087111ffd5d0e849da2be9ae5c33f
github mytty870 <otani11ace@icloud.com>
2024-05-09 11:49:38 +0900

    change logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
               height={24}
               priority
             />
-            <div>aaa</div>
+            <div>bbb</div>
           </a>
         </div>
       </div>
